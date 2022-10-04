789c45332c304ada5c13857b4e5dd17512dbceb4
StarinovOleg <hromoi@inbox.ru>
2022-10-04 15:50:18 +0700

    ui-link

--- src/screens/landing/section/Conversation.tsx
+++ src/screens/landing/section/Conversation.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import { converstation } from "../../../store/static";
 import CardListen from "../../../components/CardListen";
 import Section from "../../../components/Section";
+import Link from "../../../ui-library/Link";
 
-const cardItem=converstation.conversation.item.map((item)=>(
+const cardItem=converstation.conversation.item.slice(0, 6).map((item)=>(
 <CardListen to={item.to} title={item.title} subtitle={item.subtitle} src={item.img}   key={item.id}
 state={{
         title: `${item.title}`,
@@ -15,6 +16,13 @@ function Conversation() {
   <Section title={converstation.lessons.title}>
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-6 gap-y-6  m-10">
           {cardItem}
+    </div>
+    <div className="m-10 relative">
+      <Link to='/learning' children={converstation.landing.continue} align='align-right-side'
+        state={{
+          title: `${converstation.conversation.title}`,
+        }}
+      />
     </div>
   </Section>
   );
