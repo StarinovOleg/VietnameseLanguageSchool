20e47f499dc3f0d0f7c0b93cf2466fb3041007c1
StarinovOleg <hromoi@inbox.ru>
2022-11-22 18:25:33 +0700

    add sound files for 4 dictionary lesson

--- src/dictionary/unit-3/unit_3.ts
+++ src/dictionary/unit-3/unit_3.ts
@@ -1,128 +1,145 @@
-
+import doctor from "../../../src/assets/sounds/lesson3/doctor.mp3"
+import teacher from "../../../src/assets/sounds/lesson3/teacher.mp3"
+import nurse from "../../../src/assets/sounds/lesson3/nurse.mp3"
+import engineer from "../../../src/assets/sounds/lesson3/engineer.mp3"
+import director from "../../../src/assets/sounds/lesson3/director.mp3"
+import your_job from "../../../src/assets/sounds/lesson3/your-job.mp3"
+import do_work from "../../../src/assets/sounds/lesson3/do_work.mp3"
+import student from "../../../src/assets/sounds/lesson3/student.mp3"
+import pupil from "../../../src/assets/sounds/lesson3/pupil.mp3"
+import staff from "../../../src/assets/sounds/lesson3/staff.mp3"
+import accountant from "../../../src/assets/sounds/lesson3/accountant.mp3"
+import manager from "../../../src/assets/sounds/lesson3/manager.mp3"
+import resources from "../../../src/assets/sounds/lesson3/resources.mp3"
+import company from "../../../src/assets/sounds/lesson3/company.mp3"
+import which from "../../../src/assets/sounds/lesson3/which.mp3"
+import police from "../../../src/assets/sounds/lesson3/police.mp3"
+import businessman from "../../../src/assets/sounds/lesson3/businessman.mp3"
+import businesswoman from "../../../src/assets/sounds/lesson3/businesswoman.mp3"
 export const unit_3 = [
     {id:0,
     english:'Doctor',
     vietnamese:'Bác sĩ',
-    audio:'0',
+    audio:doctor,
     example:'0',
     picture:'1'
     },
     {id:1,
     english:'Teacher',
     vietnamese:'Giáo viên',
-    audio:'1',
+    audio:teacher,
     example:'1',
     picture:''
     },
     {id:2,
     english:'Nurse',
     vietnamese:'Y tá',
-    audio:'2',
+    audio:nurse,
     example:'2',
     picture:'2'
     },
     {id:3,
     english:'Engineer',
     vietnamese:'Kỹ sư',
-    audio:'3',
+    audio:engineer,
     example:'3',
     picture:'3'
     },
     {id:4,
     english:'Director',
     vietnamese:'Giám đốc',
-    audio:'4',
+    audio:director,
     example:'4',
     picture:'4'
     },        
     {id:5,
     english:'What is your job?',
     vietnamese:'Làm nghề gì?',
-    audio:'5',
+    audio:your_job,
     example:'5',
     picture:'5'
     },
     {id:6,
     english:'Do/work',
     vietnamese:'Làm',
-    audio:'6',
+    audio:do_work,
     example:'6',
     picture:'6'
     },
     {id:7,
     english:'Student',
     vietnamese:'Sinh viên',
-    audio:'7',
+    audio:student,
     example:'7',
     picture:'7'
     },
     {id:8,
     english:'Pupil',
     vietnamese:'Học sinh',
-    audio:'8',
+    audio:pupil,
     example:'8',
     picture:'8'
     },
     {id:9,
     english:'Staff',
     vietnamese:'Nhân viên',
-    audio:'9',
+    audio:staff,
     example:'9',
     picture:'9'
     },
     {id:10,
     english:'Accountant',
     vietnamese:'Kế toán viên',
-    audio:'',
+    audio:accountant,
     example:'',
     picture:''
     },
     {id:11,
     english:'Manager',
     vietnamese:'Quản lý',
-    audio:'',
+    audio:manager,
     example:'',
     picture:''
     },
     {id:12,
     english:'Human resources',
     vietnamese:'Nhân sự',
-    audio:'',
+    audio:resources,
     example:'',
     picture:''
     },
     {id:13,
     english:'Company',
     vietnamese:'Công ty',
-    audio:'',
+    audio:company,
     example:'',
     picture:''
     },
     {id:14,
     english:'Which',
     vietnamese:'Nào/ cái nào',
-    audio:'',
+    audio:which,
     example:'',
     picture:''
     },
     {id:15,
     english:'Police',
     vietnamese:'Công an',
-    audio:'',
+    audio:police,
     example:'',
     picture:''
     },
     {id:16,
     english:'Businessman',
     vietnamese:'Nam doanh nhân/ kinh doanh',
-    audio:'',
+    audio:businessman,
     example:'',
     picture:''
     },
     {id:17,
     english:'Businesswoman',
     vietnamese:'Nữ doanh nhân / kinh doanh',
-    audio:'',
+    audio:businesswoman,
     example:'',
     picture:''
     },
