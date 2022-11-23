d1ed87fd71ab3f4b4f3ad53380a79cf685b4dfaa
StarinovOleg <hromoi@inbox.ru>
2022-11-23 11:07:35 +0700

    add new sounds files to dictionary

--- src/dictionary/unit-6/unit_6.ts
+++ src/dictionary/unit-6/unit_6.ts
@@ -1,31 +1,61 @@
+import a_bit from "../../../src/assets/sounds/lesson6/a_bit.mp3"
+import at_by_in from "../../../src/assets/sounds/lesson6/at_by_in.mp3"
+import carefully from "../../../src/assets/sounds/lesson6/carefully.mp3"
+import cinema from "../../../src/assets/sounds/lesson6/cinema.mp3"
+import come from "../../../src/assets/sounds/lesson6/come.mp3"
+import day_month from "../../../src/assets/sounds/lesson6/day_month.mp3"
+import early_earlier from "../../../src/assets/sounds/lesson6/early_earlier.mp3"
+import free_time from "../../../src/assets/sounds/lesson6/free_time.mp3"
+import go_by_myself from "../../../src/assets/sounds/lesson6/go_by_myself.mp3"
+import half_an_hour from "../../../src/assets/sounds/lesson6/half_an_hour.mp3"
+import in_time from "../../../src/assets/sounds/lesson6/in_time.mp3"
+import late from "../../../src/assets/sounds/lesson6/late.mp3"
+import minutes from "../../../src/assets/sounds/lesson6/minutes.mp3"
+import new_week from "../../../src/assets/sounds/lesson6/new_week.mp3"
+import on_time from "../../../src/assets/sounds/lesson6/on_time.mp3"
+import pick_up from "../../../src/assets/sounds/lesson6/pick_up.mp3"
+import second from "../../../src/assets/sounds/lesson6/second.mp3"
+import see_off from "../../../src/assets/sounds/lesson6/see_off.mp3"
+import see_you_later from "../../../src/assets/sounds/lesson6/see_you_later.mp3"
+import time_hour from "../../../src/assets/sounds/lesson6/time_hour.mp3"
+import time from "../../../src/assets/sounds/lesson6/time.mp3"
+import understood from "../../../src/assets/sounds/lesson6/understood.mp3"
+import watch_movie from "../../../src/assets/sounds/lesson6/watch_movie.mp3"
+import weekday from "../../../src/assets/sounds/lesson6/weekday.mp3"
+import weekend from "../../../src/assets/sounds/lesson6/weekend.mp3"
+import what_time from "../../../src/assets/sounds/lesson6/what_time.mp3"
+import will_hold from "../../../src/assets/sounds/lesson6/will_hold.mp3"
+import wou_are_welcome from "../../../src/assets/sounds/lesson6/wou_are_welcome.mp3"
+import year from "../../../src/assets/sounds/lesson6/year.mp3"
+
 export const unit_6=[
-    {id: 0, english: "New week", vietnamese: "Tuần tới", excample:"", audio:"", picture:""},
-    {id: 1, english: "Weekday", vietnamese: "Thứ", excample:"", audio:"", picture:""},
-    {id: 2, english: "Day/month", vietnamese: "Ngày/tháng", excample:"", audio:"", picture:""},
-    {id: 3, english: "What time?", vietnamese: "Mấy giờ", excample:"", audio:"", picture:""},
-    {id: 4, english: "Year", vietnamese: "Năm", excample:"", audio:"", picture:""},
-    {id: 5, english: "Will hold", vietnamese: "Sẽ tổ chức", excample:"", audio:"", picture:""},
-    {id: 6, english: "You are welcome Nothing at all", vietnamese: "Không có gì", excample:"", audio:"", picture:""},
-    {id: 7, english: "Understood", vietnamese: "Hiểu rồi", excample:"", audio:"", picture:""},
-    {id: 8, english: "At/by/in", vietnamese: "Vào lúc/ vào ngày", excample:"", audio:"", picture:""},
-    {id: 9, english: "Weekend", vietnamese: "Cuối tuần", excample:"", audio:"", picture:""},
-    {id: 10, english: "Free time", vietnamese: "Thời gian rảnh", excample:"", audio:"", picture:""},
-    {id: 11, english: "Pick up", vietnamese: "Đón", excample:"", audio:"", picture:""},
-    {id: 12, english: "See off", vietnamese: "Tiễn", excample:"", audio:"", picture:""},
-    {id: 13, english: "Go by myself", vietnamese: "Tự đi", excample:"", audio:"", picture:""},
-    {id: 14, english: "Carefully", vietnamese: "Cẩn thận", excample:"", audio:"", picture:""},
-    {id: 15, english: "Cinema", vietnamese: "Rạp chiếu phim", excample:"", audio:"", picture:""},
-    {id: 16, english: "Watch movie/film", vietnamese: "Xam phim", excample:"", audio:"", picture:""},
-    {id: 17, english: "See you later", vietnamese: "Gặp anh sau nhé", excample:"", audio:"", picture:""},
-    {id: 18, english: "Come", vietnamese: "Đến", excample:"", audio:"", picture:""},
-    {id: 19, english: "Time/hour", vietnamese: "Giờ", excample:"", audio:"", picture:""},
-    {id: 20, english: "Minutes", vietnamese: "Phút", excample:"", audio:"", picture:""},
-    {id: 21, english: "Secound", vietnamese: "Giây", excample:"", audio:"", picture:""},
-    {id: 22, english: "Half an hour", vietnamese: "Rưỡi", excample:"", audio:"", picture:""},
-    {id: 23, english: "Time", vietnamese: "Thời gian", excample:"", audio:"", picture:""},
-    {id: 24, english: "On time", vietnamese: "Đúng giờ", excample:"", audio:"", picture:""},
-    {id: 25, english: "In time", vietnamese: "Kịp giờ", excample:"", audio:"", picture:""},
-    {id: 26, english: "Late", vietnamese: "Muộn/trễ", excample:"", audio:"", picture:""},
-    {id: 27, english: "Early/earlier", vietnamese: "Sớm/sớm hơn", excample:"", audio:"", picture:""},
-    {id: 28, english: "A bit", vietnamese: "Một chút", excample:"", audio:"", picture:""},
+    {id: 0, english: "New week", vietnamese: "Tuần tới", excample:"", audio:new_week, picture:""},
+    {id: 1, english: "Weekday", vietnamese: "Thứ", excample:"", audio:weekday, picture:""},
+    {id: 2, english: "Day/month", vietnamese: "Ngày/tháng", excample:"", audio:day_month, picture:""},
+    {id: 3, english: "What time?", vietnamese: "Mấy giờ", excample:"", audio:what_time, picture:""},
+    {id: 4, english: "Year", vietnamese: "Năm", excample:"", audio:year, picture:""},
+    {id: 5, english: "Will hold", vietnamese: "Sẽ tổ chức", excample:"", audio:will_hold, picture:""},
+    {id: 6, english: "You are welcome Nothing at all", vietnamese: "Không có gì", excample:"", audio:wou_are_welcome, picture:""},
+    {id: 7, english: "Understood", vietnamese: "Hiểu rồi", excample:"", audio:understood, picture:""},
+    {id: 8, english: "At/by/in", vietnamese: "Vào lúc/ vào ngày", excample:"", audio:at_by_in, picture:""},
+    {id: 9, english: "Weekend", vietnamese: "Cuối tuần", excample:"", audio:weekend, picture:""},
+    {id: 10, english: "Free time", vietnamese: "Thời gian rảnh", excample:"", audio:free_time, picture:""},
+    {id: 11, english: "Pick up", vietnamese: "Đón", excample:"", audio:pick_up, picture:""},
+    {id: 12, english: "See off", vietnamese: "Tiễn", excample:"", audio:see_off, picture:""},
+    {id: 13, english: "Go by myself", vietnamese: "Tự đi", excample:"", audio:go_by_myself, picture:""},
+    {id: 14, english: "Carefully", vietnamese: "Cẩn thận", excample:"", audio:carefully, picture:""},
+    {id: 15, english: "Cinema", vietnamese: "Rạp chiếu phim", excample:"", audio:cinema, picture:""},
+    {id: 16, english: "Watch movie/film", vietnamese: "Xam phim", excample:"", audio:watch_movie, picture:""},
+    {id: 17, english: "See you later", vietnamese: "Gặp anh sau nhé", excample:"", audio:see_you_later, picture:""},
+    {id: 18, english: "Come", vietnamese: "Đến", excample:"", audio:come, picture:""},
+    {id: 19, english: "Time/hour", vietnamese: "Giờ", excample:"", audio:time_hour, picture:""},
+    {id: 20, english: "Minutes", vietnamese: "Phút", excample:"", audio:minutes, picture:""},
+    {id: 21, english: "Secound", vietnamese: "Giây", excample:"", audio:second, picture:""},
+    {id: 22, english: "Half an hour", vietnamese: "Rưỡi", excample:"", audio:half_an_hour, picture:""},
+    {id: 23, english: "Time", vietnamese: "Thời gian", excample:"", audio:time, picture:""},
+    {id: 24, english: "On time", vietnamese: "Đúng giờ", excample:"", audio:on_time, picture:""},
+    {id: 25, english: "In time", vietnamese: "Kịp giờ", excample:"", audio:in_time, picture:""},
+    {id: 26, english: "Late", vietnamese: "Muộn/trễ", excample:"", audio:late, picture:""},
+    {id: 27, english: "Early/earlier", vietnamese: "Sớm/sớm hơn", excample:"", audio:early_earlier, picture:""},
+    {id: 28, english: "A bit", vietnamese: "Một chút", excample:"", audio:a_bit, picture:""},
 ]
--- src/dictionary/unit-7/unit_7.ts
+++ src/dictionary/unit-7/unit_7.ts
@@ -1,27 +1,51 @@
+import adress from "../../../src/assets/sounds/lesson7/adress.mp3"
+import be_living from "../../../src/assets/sounds/lesson7/be_living.mp3"
+import because from "../../../src/assets/sounds/lesson7/because.mp3"
+import call from "../../../src/assets/sounds/lesson7/call.mp3"
+import capital from "../../../src/assets/sounds/lesson7/capital.mp3"
+import city from "../../../src/assets/sounds/lesson7/city.mp3"
+import contact from "../../../src/assets/sounds/lesson7/contact.mp3"
+import convinient from "../../../src/assets/sounds/lesson7/convinient.mp3"
+import enroll from "../../../src/assets/sounds/lesson7/enroll.mp3"
+import inform from "../../../src/assets/sounds/lesson7/inform.mp3"
+import it_is_ok from "../../../src/assets/sounds/lesson7/it_is_ok.mp3"
+import living from "../../../src/assets/sounds/lesson7/living.mp3"
+import me_too from "../../../src/assets/sounds/lesson7/me_too.mp3"
+import now from "../../../src/assets/sounds/lesson7/now.mp3"
+import of_cua from "../../../src/assets/sounds/lesson7/of_cua.mp3"
+import older_than from "../../../src/assets/sounds/lesson7/older_than.mp3"
+import phone_number from "../../../src/assets/sounds/lesson7/phone_number.mp3"
+import quarter from "../../../src/assets/sounds/lesson7/quarter.mp3"
+import respect from "../../../src/assets/sounds/lesson7/respect.mp3"
+import schools_headmaster from "../../../src/assets/sounds/lesson7/schools_headmaster.mp3"
+import street from "../../../src/assets/sounds/lesson7/street.mp3"
+import when_you_born from "../../../src/assets/sounds/lesson7/when_you_born.mp3"
+import you_are_welcome from "../../../src/assets/sounds/lesson7/you_are_welcome.mp3"
+import you_i from "../../../src/assets/sounds/lesson7/you_i.mp3"
+
 export const unit_7=[
-    {id: 0, english: "You/I", vietnamese: "Bạn /Mình", excample:"", audio:"", picture:""},
-    {id: 1, english: "When were you born?", vietnamese: "Bạn sinh năm nào? khi nào", excample:"", audio:"", picture:""},
-    {id: 2, english: "Are you yah! (compliment polite& respectful sentences)", vietnamese: "", excample:"", audio:"", picture:""},
-    {id: 3, english: "Call", vietnamese: "Gọi", excample:"", audio:"", picture:""},
-    {id: 4, english: "Because", vietnamese: "Vì", excample:"", audio:"", picture:""},
-    {id: 5, english: "Older than", vietnamese: "Hơn tuổi", excample:"", audio:"", picture:""},
-    {id: 6, english: "it is ok", vietnamese: "Vậy cũng được", excample:"", audio:"", picture:""},
-    {id: 7, english: "Where are you living?/ where do you live?", vietnamese: "Bạn đang sống ở đâu ạ?", excample:"", audio:"", picture:""},
-    {id: 8, english: "(Be) living", vietnamese: "Đang sống", excample:"", audio:"", picture:""},
-    {id: 9, english: "Me too/so do I!", vietnamese: "Cũng vậy ạ", excample:"", audio:"", picture:""},
-    {id: 10, english: "You are welcome", vietnamese: "Không có chi", excample:"", audio:"", picture:""},
-    {id: 11, english: "Enroll/register", vietnamese: "Đăng ký", excample:"", audio:"", picture:""},
-    {id: 12, english: "Address", vietnamese: "̣̣Địa chỉ", excample:"", audio:"", picture:""},
-    {id: 13, english: "Telephone phone number", vietnamese: "Số điện thoại", excample:"", audio:"", picture:""},
-    {id: 14, english: "Now", vietnamese: "Hiện nay", excample:"", audio:"", picture:""},
-    {id: 15, english: "Street", vietnamese: "Đường phố", excample:"", audio:"", picture:""},
-    {id: 16, english: "Quater/District", vietnamese: "Quận", excample:"", audio:"", picture:""},
-    {id: 17, english: "City", vietnamese: "Thành phố", excample:"", audio:"", picture:""},
-    {id: 18, english: "Capital", vietnamese: "Thủ đô", excample:"", audio:"", picture:""},
-    {id: 19, english: "Convenient", vietnamese: "Tiện/thuận lợi", excample:"", audio:"", picture:""},
-    {id: 20, english: "Contact", vietnamese: "Liên lạc", excample:"", audio:"", picture:""},
-    {id: 21, english: "Inform", vietnamese: "Thông báo", excample:"", audio:"", picture:""},
-    {id: 22, english: "Schools headmaster", vietnamese: "Hiệu trưởng trường", excample:"", audio:"", picture:""},
-    {id: 23, english: "Students are called as to sho their good manners to their teacher", vietnamese: "Con", excample:"", audio:"", picture:""},
-    {id: 24, english: "Of", vietnamese: "Của", excample:"", audio:"", picture:""},
+    {id: 0, english: "You/I", vietnamese: "Bạn /Mình", excample:"", audio:you_i, picture:""},
+    {id: 1, english: "When were you born?", vietnamese: "Bạn sinh năm nào? khi nào", excample:"", audio:when_you_born, picture:""},
+    {id: 2, english: "Are you yah! (compliment polite& respectful sentences)", vietnamese: "Ấu nhỉ", excample:"", audio:respect, picture:""},
+    {id: 3, english: "Call", vietnamese: "Gọi", excample:"", audio:call, picture:""},
+    {id: 4, english: "Because", vietnamese: "Vì", excample:"", audio:because, picture:""},
+    {id: 5, english: "Older than", vietnamese: "Hơn tuổi", excample:"", audio:older_than, picture:""},
+    {id: 6, english: "it is ok", vietnamese: "Vậy cũng được", excample:"", audio:it_is_ok, picture:""},
+    {id: 7, english: "Where are you living?/ where do you live?", vietnamese: "Bạn đang sống ở đâu ạ?", excample:"", audio:living, picture:""},
+    {id: 8, english: "(Be) living", vietnamese: "Đang sống", excample:"", audio:be_living, picture:""},
+    {id: 9, english: "Me too/so do I!", vietnamese: "Cũng vậy ạ", excample:"", audio:me_too, picture:""},
+    {id: 10, english: "You are welcome", vietnamese: "Không có chi", excample:"", audio:you_are_welcome, picture:""},
+    {id: 11, english: "Enroll/register", vietnamese: "Đăng ký", excample:"", audio:enroll, picture:""},
+    {id: 12, english: "Address", vietnamese: "̣̣Địa chỉ", excample:"", audio:adress, picture:""},
+    {id: 13, english: "Telephone phone number", vietnamese: "Số điện thoại", excample:"", audio:phone_number, picture:""},
+    {id: 14, english: "Now", vietnamese: "Hiện nay", excample:"", audio:now, picture:""},
+    {id: 15, english: "Street", vietnamese: "Đường phố", excample:"", audio:street, picture:""},
+    {id: 16, english: "Quater/District", vietnamese: "Quận", excample:"", audio:quarter, picture:""},
+    {id: 17, english: "City", vietnamese: "Thành phố", excample:"", audio:city, picture:""},
+    {id: 18, english: "Capital", vietnamese: "Thủ đô", excample:"", audio:capital, picture:""},
+    {id: 19, english: "Convenient", vietnamese: "Tiện/thuận lợi", excample:"", audio:convinient, picture:""},
+    {id: 20, english: "Contact", vietnamese: "Liên lạc", excample:"", audio:contact, picture:""},
+    {id: 21, english: "Inform", vietnamese: "Thông báo", excample:"", audio:inform, picture:""},
+    {id: 22, english: "Schools headmaster", vietnamese: "Hiệu trưởng trường", excample:"", audio:schools_headmaster, picture:""},
+    {id: 24, english: "Of", vietnamese: "Của", excample:"", audio:of_cua, picture:""},
 ]
