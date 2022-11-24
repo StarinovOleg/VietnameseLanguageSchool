410d41b21e746856238d4930e3ff60ef911891e0
StarinovOleg <hromoi@inbox.ru>
2022-11-24 15:36:52 +0700

    add lessons sound part 2

--- src/dictionary/unit-13/unit_13.ts
+++ src/dictionary/unit-13/unit_13.ts
@@ -1,22 +1,43 @@
+import about from "../../../src/assets/sounds/lesson13/about.mp3"
+import advice from "../../../src/assets/sounds/lesson13/advice.mp3"
+import ask from "../../../src/assets/sounds/lesson13/ask.mp3"
+import can from "../../../src/assets/sounds/lesson13/can.mp3"
+import dear_darling from "../../../src/assets/sounds/lesson13/dear_darling.mp3"
+import difficult from "../../../src/assets/sounds/lesson13/difficult.mp3"
+import first from "../../../src/assets/sounds/lesson13/first.mp3"
+import go_straight from "../../../src/assets/sounds/lesson13/go_straight.mp3"
+import hand_in from "../../../src/assets/sounds/lesson13/hand_in.mp3"
+import homework from "../../../src/assets/sounds/lesson13/homework.mp3"
+import little from "../../../src/assets/sounds/lesson13/little.mp3"
+import many from "../../../src/assets/sounds/lesson13/many.mp3"
+import propose from "../../../src/assets/sounds/lesson13/propose.mp3"
+import really from "../../../src/assets/sounds/lesson13/really.mp3"
+import show from "../../../src/assets/sounds/lesson13/show.mp3"
+import suggest from "../../../src/assets/sounds/lesson13/suggest.mp3"
+import take_care from "../../../src/assets/sounds/lesson13/take_care.mp3"
+import turn_left from "../../../src/assets/sounds/lesson13/turn_left.mp3"
+import turn_right from "../../../src/assets/sounds/lesson13/turn_right.mp3"
+import way from "../../../src/assets/sounds/lesson13/way.mp3"
+
 export const unit_13=[
-    {id: 0, english: "Can", vietnamese: "Có thể", excample:"", audio:"", picture:""},
-    {id: 1, english: "Go straight", vietnamese: "Đi thẳng", excample:"", audio:"", picture:""},
-    {id: 2, english: "Turn right", vietnamese: "Rẽ/ phải", excample:"", audio:"", picture:""},
-    {id: 3, english: "Turn left", vietnamese: "Rẽ /trái", excample:"", audio:"", picture:""},
-    {id: 4, english: "About/ approximately", vietnamese: "Khoảng", excample:"", audio:"", picture:""},
-    {id: 5, english: "Way", vietnamese: "Đường / cách", excample:"", audio:"", picture:""},
-    {id: 6, english: "Little /a bit", vietnamese: "Một chút", excample:"", audio:"", picture:""},
-    {id: 7, english: "Ask", vietnamese: "Hỏi", excample:"", audio:"", picture:""},
-    {id: 8, english: "Take care on your way", vietnamese: "Đi cẩn thận / hãy giữ gìn nhé", excample:"", audio:"", picture:""},
-    {id: 9, english: "Show", vietnamese: "Chỉ /bảo", excample:"", audio:"", picture:""},
-    {id: 10, english: "Dear darling", vietnamese: "Con yêu", excample:"", audio:"", picture:""},
-    {id: 11, english: "Hand in", vietnamese: "Nộp", excample:"", audio:"", picture:""},
-    {id: 12, english: "Many", vietnamese: "Nhiều", excample:"", audio:"", picture:""},
-    {id: 13, english: "Homework", vietnamese: "Bài tập", excample:"", audio:"", picture:""},
-    {id: 14, english: "Advice", vietnamese: "Khuyên bảo", excample:"", audio:"", picture:""},
-    {id: 15, english: "Propose /reccomend", vietnamese: "Đề nghị", excample:"", audio:"", picture:""},
-    {id: 16, english: "First", vietnamese: "Trước", excample:"", audio:"", picture:""},
-    {id: 17, english: "Really", vietnamese: "Vậy sao /thật à", excample:"", audio:"", picture:""},
-    {id: 18, english: "Suggest", vietnamese: "Gợi ý", excample:"", audio:"", picture:""},
-    {id: 19, english: "Difficult math", vietnamese: "Bài toán khó", excample:"", audio:"", picture:""},
+    {id: 0, english: "Can", vietnamese: "Có thể", excample:"", audio:can, picture:""},
+    {id: 1, english: "Go straight", vietnamese: "Đi thẳng", excample:"", audio:go_straight, picture:""},
+    {id: 2, english: "Turn right", vietnamese: "Rẽ/ phải", excample:"", audio:turn_right, picture:""},
+    {id: 3, english: "Turn left", vietnamese: "Rẽ /trái", excample:"", audio:turn_left, picture:""},
+    {id: 4, english: "About/ approximately", vietnamese: "Khoảng", excample:"", audio:about, picture:""},
+    {id: 5, english: "Way", vietnamese: "Đường / cách", excample:"", audio:way, picture:""},
+    {id: 6, english: "Little /a bit", vietnamese: "Một chút", excample:"", audio:little, picture:""},
+    {id: 7, english: "Ask", vietnamese: "Hỏi", excample:"", audio:ask, picture:""},
+    {id: 8, english: "Take care on your way", vietnamese: "Đi cẩn thận / hãy giữ gìn nhé", excample:"", audio:take_care, picture:""},
+    {id: 9, english: "Show", vietnamese: "Chỉ /bảo", excample:"", audio:show, picture:""},
+    {id: 10, english: "Dear darling", vietnamese: "Con yêu", excample:"", audio:dear_darling, picture:""},
+    {id: 11, english: "Hand in", vietnamese: "Nộp", excample:"", audio:hand_in, picture:""},
+    {id: 12, english: "Many", vietnamese: "Nhiều", excample:"", audio:many, picture:""},
+    {id: 13, english: "Homework", vietnamese: "Bài tập", excample:"", audio:homework, picture:""},
+    {id: 14, english: "Advice", vietnamese: "Khuyên bảo", excample:"", audio:advice, picture:""},
+    {id: 15, english: "Propose /reccomend", vietnamese: "Đề nghị", excample:"", audio:propose, picture:""},
+    {id: 16, english: "First", vietnamese: "Trước", excample:"", audio:first, picture:""},
+    {id: 17, english: "Really", vietnamese: "Vậy sao /thật à", excample:"", audio:really, picture:""},
+    {id: 18, english: "Suggest", vietnamese: "Gợi ý", excample:"", audio:suggest, picture:""},
+    {id: 19, english: "Difficult math", vietnamese: "Bài toán khó", excample:"", audio:difficult, picture:""},
 ]
