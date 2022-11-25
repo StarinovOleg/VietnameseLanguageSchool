08ae6a371cc01dfd24b2786246d730420e4f799d
StarinovOleg <hromoi@inbox.ru>
2022-11-25 09:30:34 +0700

    add dictionary sound part3

--- src/dictionary/unit-11/unit_11.ts
+++ src/dictionary/unit-11/unit_11.ts
@@ -1,71 +1,139 @@
+import almost from "../../../src/assets/sounds/lesson11/almost.mp3"
+import apple from "../../../src/assets/sounds/lesson11/apple.mp3"
+import arrange from "../../../src/assets/sounds/lesson11/arrange.mp3"
+import belly from "../../../src/assets/sounds/lesson11/belly.mp3"
+import bring from "../../../src/assets/sounds/lesson11/bring.mp3"
+import buy from "../../../src/assets/sounds/lesson11/buy.mp3"
+import churish from "../../../src/assets/sounds/lesson11/churish.mp3"
+import clatter from "../../../src/assets/sounds/lesson11/clatter.mp3"
+import coddle from "../../../src/assets/sounds/lesson11/coddle.mp3"
+import confused from "../../../src/assets/sounds/lesson11/confused.mp3"
+import curl from "../../../src/assets/sounds/lesson11/curl.mp3"
+import curt from "../../../src/assets/sounds/lesson11/curt.mp3"
+import discuss from "../../../src/assets/sounds/lesson11/discuss.mp3"
+import each_region from "../../../src/assets/sounds/lesson11/each_region.mp3"
+import easy_going from "../../../src/assets/sounds/lesson11/easy_going.mp3"
+import enought from "../../../src/assets/sounds/lesson11/enought.mp3"
+import feeling_ashamed from "../../../src/assets/sounds/lesson11/feeling_ashamed.mp3"
+import fidgety from "../../../src/assets/sounds/lesson11/fidgety.mp3"
+import flagging from "../../../src/assets/sounds/lesson11/flagging.mp3"
+import fret from "../../../src/assets/sounds/lesson11/fret.mp3"
+import glimmering from "../../../src/assets/sounds/lesson11/glimmering.mp3"
+import go_with from "../../../src/assets/sounds/lesson11/go_with.mp3"
+import grumble from "../../../src/assets/sounds/lesson11/grumble.mp3"
+import harrying from "../../../src/assets/sounds/lesson11/harrying.mp3"
+import higher from "../../../src/assets/sounds/lesson11/higher.mp3"
+import hotter from "../../../src/assets/sounds/lesson11/hotter.mp3"
+import impetuous from "../../../src/assets/sounds/lesson11/impetuous.mp3"
+import imposing from "../../../src/assets/sounds/lesson11/imposing.mp3"
+import interlace from "../../../src/assets/sounds/lesson11/interlace.mp3"
+import interminable from "../../../src/assets/sounds/lesson11/interminable.mp3"
+import irritate from "../../../src/assets/sounds/lesson11/irritate.mp3"
+import lose_temper from "../../../src/assets/sounds/lesson11/lose_temper.mp3"
+import mango from "../../../src/assets/sounds/lesson11/mango.mp3"
+import matured from "../../../src/assets/sounds/lesson11/matured.mp3"
+import messy from "../../../src/assets/sounds/lesson11/messy.mp3"
+import mid_region from "../../../src/assets/sounds/lesson11/mid_region.mp3"
+import more from "../../../src/assets/sounds/lesson11/more.mp3"
+import most from "../../../src/assets/sounds/lesson11/most.mp3"
+import neglect from "../../../src/assets/sounds/lesson11/neglect.mp3"
+import north_central_region from "../../../src/assets/sounds/lesson11/north_central_region.mp3"
+import nothern from "../../../src/assets/sounds/lesson11/nothern.mp3"
+import only from "../../../src/assets/sounds/lesson11/only.mp3"
+import orange from "../../../src/assets/sounds/lesson11/orange.mp3"
+import persistent from "../../../src/assets/sounds/lesson11/persistent.mp3"
+import pleasant from "../../../src/assets/sounds/lesson11/pleasant.mp3"
+import reiterate from "../../../src/assets/sounds/lesson11/reiterate.mp3"
+import repent from "../../../src/assets/sounds/lesson11/repent.mp3"
+import rude from "../../../src/assets/sounds/lesson11/rude.mp3"
+import scowl from "../../../src/assets/sounds/lesson11/scowl.mp3"
+import should from "../../../src/assets/sounds/lesson11/should.mp3"
+import slowly from "../../../src/assets/sounds/lesson11/slowly.mp3"
+import solicit from "../../../src/assets/sounds/lesson11/solicit.mp3"
+import south_central_region from "../../../src/assets/sounds/lesson11/south_central_region.mp3"
+import southern from "../../../src/assets/sounds/lesson11/southern.mp3"
+import species from "../../../src/assets/sounds/lesson11/species.mp3"
+import store from "../../../src/assets/sounds/lesson11/store.mp3"
+import suffuse from "../../../src/assets/sounds/lesson11/suffuse.mp3"
+import sure from "../../../src/assets/sounds/lesson11/sure.mp3"
+import telling_tales from "../../../src/assets/sounds/lesson11/telling_tales.mp3"
+import thailand from "../../../src/assets/sounds/lesson11/thailand.mp3"
+import this_time from "../../../src/assets/sounds/lesson11/this_time.mp3"
+import toddling from "../../../src/assets/sounds/lesson11/toddling.mp3"
+import twilight from "../../../src/assets/sounds/lesson11/twilight.mp3"
+import uneasy from "../../../src/assets/sounds/lesson11/uneasy.mp3"
+import visit from "../../../src/assets/sounds/lesson11/visit.mp3"
+import warm from "../../../src/assets/sounds/lesson11/warm.mp3"
+import warmer from "../../../src/assets/sounds/lesson11/warmer.mp3"
+import western from "../../../src/assets/sounds/lesson11/western.mp3"
 export const unit_11=[
-    {id: 0, english: "Manage/arrange", vietnamese: "Thu xếp", excample:"", audio:"", picture:""},
-    {id: 1, english: "Go with", vietnamese: "Đi cùng", excample:"", audio:"", picture:""},
-    {id: 2, english: "Visit", vietnamese: "Thăm", excample:"", audio:"", picture:""},
-    {id: 3, english: "Almost", vietnamese: "Hầu như", excample:"", audio:"", picture:""},
-    {id: 4, english: "More", vietnamese: "Hơn", excample:"", audio:"", picture:""},
-    {id: 5, english: "In this time", vietnamese: "Vào thời điểm này", excample:"", audio:"", picture:""},
-    {id: 6, english: "Should", vietnamese: "Nên", excample:"", audio:"", picture:""},
-    {id: 7, english: "Enought", vietnamese: "Đủ", excample:"", audio:"", picture:""},
-    {id: 8, english: "Mid-region", vietnamese: "Miền Trung", excample:"", audio:"", picture:""},
-    {id: 9, english: "Bring", vietnamese: "Mang", excample:"", audio:"", picture:""},
-    {id: 10, english: "Warmer", vietnamese: "Ấm hơn", excample:"", audio:"", picture:""},
-    {id: 11, english: "Higher", vietnamese: "Cao hơn", excample:"", audio:"", picture:""},
-    {id: 12, english: "Hotter", vietnamese: "Nóng hơn", excample:"", audio:"", picture:""},
-    {id: 13, english: "Each region", vietnamese: "Từng vùng", excample:"", audio:"", picture:""},
-    {id: 14, english: "Western", vietnamese: "Miền Tây", excample:"", audio:"", picture:""},
-    {id: 15, english: "Southern", vietnamese: "Miền Nam", excample:"", audio:"", picture:""},
-    {id: 16, english: "Nothern", vietnamese: "Miền Bắc", excample:"", audio:"", picture:""},
-    {id: 17, english: "North central region", vietnamese: "Miền Bắc Trung Bộ", excample:"", audio:"", picture:""},
-    {id: 18, english: "South central region", vietnamese: "Nam Trung Bộ", excample:"", audio:"", picture:""},
-    {id: 19, english: "Buy", vietnamese: "Mua", excample:"", audio:"", picture:""},
-    {id: 20, english: "Orange", vietnamese: "Cam", excample:"", audio:"", picture:""},
-    {id: 21, english: "Apple", vietnamese: "Táo", excample:"", audio:"", picture:""},
-    {id: 22, english: "Only", vietnamese: "Chỉ", excample:"", audio:"", picture:""},
-    {id: 23, english: "Mango", vietnamese: "Xoài", excample:"", audio:"", picture:""},
-    {id: 24, english: "Store", vietnamese: "Cửa hàng", excample:"", audio:"", picture:""},
-    {id: 25, english: "Species", vietnamese: "Loại", excample:"", audio:"", picture:""},
-    {id: 26, english: "Most/adj+est", vietnamese: "Nhất", excample:"", audio:"", picture:""},
-    {id: 27, english: "Thailand", vietnamese: "Thái", excample:"", audio:"", picture:""},
-    {id: 28, english: "Glimmering", vietnamese: "Anh ánh", excample:"", audio:"", picture:""},
+    {id: 0, english: "Manage/arrange", vietnamese: "Thu xếp", excample:"", audio:arrange, picture:""},
+    {id: 1, english: "Go with", vietnamese: "Đi cùng", excample:"", audio:go_with, picture:""},
+    {id: 2, english: "Visit", vietnamese: "Thăm", excample:"", audio:visit, picture:""},
+    {id: 3, english: "Almost", vietnamese: "Hầu như", excample:"", audio:almost, picture:""},
+    {id: 4, english: "More", vietnamese: "Hơn", excample:"", audio:more, picture:""},
+    {id: 5, english: "In this time", vietnamese: "Vào thời điểm này", excample:"", audio:this_time, picture:""},
+    {id: 6, english: "Should", vietnamese: "Nên", excample:"", audio:should, picture:""},
+    {id: 7, english: "Enought", vietnamese: "Đủ", excample:"", audio:enought, picture:""},
+    {id: 8, english: "Mid-region", vietnamese: "Miền Trung", excample:"", audio:mid_region, picture:""},
+    {id: 9, english: "Bring", vietnamese: "Mang", excample:"", audio:bring, picture:""},
+    {id: 10, english: "Warmer", vietnamese: "Ấm hơn", excample:"", audio:warmer, picture:""},
+    {id: 11, english: "Higher", vietnamese: "Cao hơn", excample:"", audio:higher, picture:""},
+    {id: 12, english: "Hotter", vietnamese: "Nóng hơn", excample:"", audio:hotter, picture:""},
+    {id: 13, english: "Each region", vietnamese: "Từng vùng", excample:"", audio:each_region, picture:""},
+    {id: 14, english: "Western", vietnamese: "Miền Tây", excample:"", audio:western, picture:""},
+    {id: 15, english: "Southern", vietnamese: "Miền Nam", excample:"", audio:southern, picture:""},
+    {id: 16, english: "Nothern", vietnamese: "Miền Bắc", excample:"", audio:nothern, picture:""},
+    {id: 17, english: "North central region", vietnamese: "Miền Bắc Trung Bộ", excample:"", audio:north_central_region, picture:""},
+    {id: 18, english: "South central region", vietnamese: "Nam Trung Bộ", excample:"", audio:south_central_region, picture:""},
+    {id: 19, english: "Buy", vietnamese: "Mua", excample:"", audio:buy, picture:""},
+    {id: 20, english: "Orange", vietnamese: "Cam", excample:"", audio:orange, picture:""},
+    {id: 21, english: "Apple", vietnamese: "Táo", excample:"", audio:apple, picture:""},
+    {id: 22, english: "Only", vietnamese: "Chỉ", excample:"", audio:only, picture:""},
+    {id: 23, english: "Mango", vietnamese: "Xoài", excample:"", audio:mango, picture:""},
+    {id: 24, english: "Store", vietnamese: "Cửa hàng", excample:"", audio:store, picture:""},
+    {id: 25, english: "Species", vietnamese: "Loại", excample:"", audio:species, picture:""},
+    {id: 26, english: "Most/adj+est", vietnamese: "Nhất", excample:"", audio:most, picture:""},
+    {id: 27, english: "Thailand", vietnamese: "Thái", excample:"", audio:thailand, picture:""},
+    {id: 28, english: "Glimmering", vietnamese: "Anh ánh", excample:"", audio:glimmering, picture:""},
 
-    {id: 29, english: "Impetuous", vietnamese: "Ào ào", excample:"", audio:"", picture:""},
-    {id: 30, english: "Uneasy", vietnamese: "Áy náy", excample:"", audio:"", picture:""},
-    {id: 31, english: "Repent", vietnamese: "Ăn năn", excample:"", audio:"", picture:""},
-    {id: 32, english: "Warm", vietnamese: "Âm ấm", excample:"", audio:"", picture:""},
-    {id: 33, english: "Discuss", vietnamese: "Bàn bạc", excample:"", audio:"", picture:""},
-    {id: 34, english: "Swollen belly", vietnamese: "Bệ sệ", excample:"", audio:"", picture:""},
-    {id: 35, english: "Feeling ashamed", vietnamese: "Bẽ bàng", excample:"", audio:"", picture:""},
-    {id: 36, english: "Telling tales", vietnamese: "Bép xép", excample:"", audio:"", picture:""},
-    {id: 37, english: "Neglect", vietnamese: "Bỏ bê", excample:"", audio:"", picture:""},
-    {id: 38, english: "Fret", vietnamese: "Bổi hổi", excample:"", audio:"", picture:""},
-    {id: 39, english: "Confused", vietnamese: "Bổi rối", excample:"", audio:"", picture:""},
-    {id: 40, english: "Flagging", vietnamese: "Bủn rủn", excample:"", audio:"", picture:""},
-    {id: 41, english: "Messy", vietnamese: "Bừa bãi", excample:"", audio:"", picture:""},
-    {id: 42, english: "Fidgety", vietnamese: "Bứt rứt", excample:"", audio:"", picture:""},
-    {id: 43, english: "Harrying", vietnamese: "Canh cánh", excample:"", audio:"", picture:""},
-    {id: 44, english: "Scowl", vietnamese: "Cau có", excample:"", audio:"", picture:""},
-    {id: 45, english: "Irritate", vietnamese: "Cau nhàu", excample:"", audio:"", picture:""},
-    {id: 46, english: "Clatter", vietnamese: "Cầm cập", excample:"", audio:"", picture:""},
-    {id: 47, english: "Grumble", vietnamese: "Cằn nhằn", excample:"", audio:"", picture:""},
-    {id: 48, english: "Twilight", vietnamese: "Ấm hơn", excample:"", audio:"", picture:""},
-    {id: 49, english: "Sure", vietnamese: "Chắc chắn", excample:"", audio:"", picture:""},
-    {id: 50, english: "Suffuse", vietnamese: "Chan chứa", excample:"", audio:"", picture:""},
-    {id: 51, english: "Solicit", vietnamese: "Chạy chọt", excample:"", audio:"", picture:""},
-    {id: 52, english: "Interlace", vietnamese: "Chằng chịt", excample:"", audio:"", picture:""},
-    {id: 53, english: "Imposing", vietnamese: "Chễm chệ", excample:"", audio:"", picture:""},
-    {id: 54, english: "Slowly", vietnamese: "Chầm chậm", excample:"", audio:"", picture:""},
-    {id: 55, english: "Toddling", vietnamese: "Chập chững", excample:"", audio:"", picture:""},
-    {id: 56, english: "Coddle", vietnamese: "Chiều chuộng", excample:"", audio:"", picture:""},
-    {id: 57, english: "Matured", vietnamese: "Chín chắn", excample:"", audio:"", picture:""},
-    {id: 58, english: "Rude", vietnamese: "Cộc cằn", excample:"", audio:"", picture:""},
-    {id: 59, english: "Curt", vietnamese: "Cộc lốc", excample:"", audio:"", picture:""},
-    {id: 60, english: "Lose temper", vietnamese: "Cuống cuồng", excample:"", audio:"", picture:""},
-    {id: 61, english: "Curl", vietnamese: "Cuồn cuộn", excample:"", audio:"", picture:""},
-    {id: 62, english: "Interminable", vietnamese: "Dằng dặc", excample:"", audio:"", picture:""},
-    {id: 63, english: "Pleasant", vietnamese: "Êm dềm", excample:"", audio:"", picture:""},
-    {id: 64, english: "Persistent", vietnamese: "Gan góc", excample:"", audio:"", picture:""},
-    {id: 65, english: "Reiterate", vietnamese: "Ngờ ngợ", excample:"", audio:"", picture:""},
-    {id: 66, english: "Churish", vietnamese: "Cắ́t gỏng", excample:"", audio:"", picture:""},
-    {id: 67, english: "Easy going", vietnamese: "Cần gũì", excample:"", audio:"", picture:""},
+    {id: 29, english: "Impetuous", vietnamese: "Ào ào", excample:"", audio:impetuous, picture:""},
+    {id: 30, english: "Uneasy", vietnamese: "Áy náy", excample:"", audio:uneasy, picture:""},
+    {id: 31, english: "Repent", vietnamese: "Ăn năn", excample:"", audio:repent, picture:""},
+    {id: 32, english: "Warm", vietnamese: "Âm ấm", excample:"", audio:warm, picture:""},
+    {id: 33, english: "Discuss", vietnamese: "Bàn bạc", excample:"", audio:discuss, picture:""},
+    {id: 34, english: "Swollen belly", vietnamese: "Bệ sệ", excample:"", audio:belly, picture:""},
+    {id: 35, english: "Feeling ashamed", vietnamese: "Bẽ bàng", excample:"", audio:feeling_ashamed, picture:""},
+    {id: 36, english: "Telling tales", vietnamese: "Bép xép", excample:"", audio:telling_tales, picture:""},
+    {id: 37, english: "Neglect", vietnamese: "Bỏ bê", excample:"", audio:neglect, picture:""},
+    {id: 38, english: "Fret", vietnamese: "Bổi hổi", excample:"", audio:fret, picture:""},
+    {id: 39, english: "Confused", vietnamese: "Bổi rối", excample:"", audio:confused, picture:""},
+    {id: 40, english: "Flagging", vietnamese: "Bủn rủn", excample:"", audio:flagging, picture:""},
+    {id: 41, english: "Messy", vietnamese: "Bừa bãi", excample:"", audio:messy, picture:""},
+    {id: 42, english: "Fidgety", vietnamese: "Bứt rứt", excample:"", audio:fidgety, picture:""},
+    {id: 43, english: "Harrying", vietnamese: "Canh cánh", excample:"", audio:harrying, picture:""},
+    {id: 44, english: "Scowl", vietnamese: "Cau có", excample:"", audio:scowl, picture:""},
+    {id: 45, english: "Irritate", vietnamese: "Cau nhàu", excample:"", audio:irritate, picture:""},
+    {id: 46, english: "Clatter", vietnamese: "Cầm cập", excample:"", audio:clatter, picture:""},
+    {id: 47, english: "Grumble", vietnamese: "Cằn nhằn", excample:"", audio:grumble, picture:""},
+    {id: 48, english: "Twilight", vietnamese: "Ấm hơn", excample:"", audio:twilight, picture:""},
+    {id: 49, english: "Sure", vietnamese: "Chắc chắn", excample:"", audio:sure, picture:""},
+    {id: 50, english: "Suffuse", vietnamese: "Chan chứa", excample:"", audio:suffuse, picture:""},
+    {id: 51, english: "Solicit", vietnamese: "Chạy chọt", excample:"", audio:solicit, picture:""},
+    {id: 52, english: "Interlace", vietnamese: "Chằng chịt", excample:"", audio:interlace, picture:""},
+    {id: 53, english: "Imposing", vietnamese: "Chễm chệ", excample:"", audio:imposing, picture:""},
+    {id: 54, english: "Slowly", vietnamese: "Chầm chậm", excample:"", audio:slowly, picture:""},
+    {id: 55, english: "Toddling", vietnamese: "Chập chững", excample:"", audio:toddling, picture:""},
+    {id: 56, english: "Coddle", vietnamese: "Chiều chuộng", excample:"", audio:coddle, picture:""},
+    {id: 57, english: "Matured", vietnamese: "Chín chắn", excample:"", audio:matured, picture:""},
+    {id: 58, english: "Rude", vietnamese: "Cộc cằn", excample:"", audio:rude, picture:""},
+    {id: 59, english: "Curt", vietnamese: "Cộc lốc", excample:"", audio:curt, picture:""},
+    {id: 60, english: "Lose temper", vietnamese: "Cuống cuồng", excample:"", audio:lose_temper, picture:""},
+    {id: 61, english: "Curl", vietnamese: "Cuồn cuộn", excample:"", audio:curl, picture:""},
+    {id: 62, english: "Interminable", vietnamese: "Dằng dặc", excample:"", audio:interminable, picture:""},
+    {id: 63, english: "Pleasant", vietnamese: "Êm dềm", excample:"", audio:pleasant, picture:""},
+    {id: 64, english: "Persistent", vietnamese: "Gan góc", excample:"", audio:persistent, picture:""},
+    {id: 65, english: "Reiterate", vietnamese: "Ngờ ngợ", excample:"", audio:reiterate, picture:""},
+    {id: 66, english: "Churish", vietnamese: "Cắ́t gỏng", excample:"", audio:churish, picture:""},
+    {id: 67, english: "Easy going", vietnamese: "Cần gũì", excample:"", audio:easy_going, picture:""},
 ]
