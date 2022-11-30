140c4138edf1fe11a16286c33ac9333c9516027d
StarinovOleg <hromoi@inbox.ru>
2022-11-30 13:51:11 +0700

    hide few elements for mobile version

--- src/screens/words/components/DisplayWordBlock.tsx
+++ src/screens/words/components/DisplayWordBlock.tsx
@@ -11,10 +11,10 @@ function DisplayWordBlock(props: {
   return (
     <>
       <div className="leading-[3rem]">{props.word}</div>
-      <div className="inline-block pt-10">
+      <div className="inline-block pt-10 hidden lg:block md:block">
         <img src={props.picture} />
       </div>
-      <div className="text-blue-500 p-10">
+      <div className="text-blue-500 p-10 hidden lg:block md:block">
         <span className="text-xl p-2">{props.example}</span>
       </div>
       <ButtonSound onClick={props.onClick} />
