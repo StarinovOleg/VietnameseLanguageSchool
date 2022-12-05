3ec674f855ae17feb30bf97618c5aaaf7e5e8198
StarinovOleg <hromoi@inbox.ru>
2022-12-05 15:59:55 +0700

    add pic to unit-3

--- src/dictionary/unit-3/unit_3.ts
+++ src/dictionary/unit-3/unit_3.ts
@@ -16,131 +16,151 @@ import which from "../../../src/assets/sounds/lesson3/which.mp3"
 import police from "../../../src/assets/sounds/lesson3/police.mp3"
 import businessman from "../../../src/assets/sounds/lesson3/businessman.mp3"
 import businesswoman from "../../../src/assets/sounds/lesson3/businesswoman.mp3"
+
+import doctor_pic from "../../../src/assets/lesson3/doctor.png"
+import teacher_pic from "../../../src/assets/lesson3/teacher.png"
+import nurse_pic from "../../../src/assets/lesson3/nurse.png"
+import engineer_pic from "../../../src/assets/lesson3/engineer.png"
+import director_pic from "../../../src/assets/lesson3/director.png"
+import your_job_pic from "../../../src/assets/lesson3/what_ur_job.png"
+import do_work_pic from "../../../src/assets/lesson3/do.png"
+import student_pic from "../../../src/assets/lesson3/student.png"
+import pupil_pic from "../../../src/assets/lesson3/pupil.png"
+import staff_pic from "../../../src/assets/lesson3/staff.png"
+import accountant_pic from "../../../src/assets/lesson3/accountant.png"
+import manager_pic from "../../../src/assets/lesson3/manager.png"
+import resources_pic from "../../../src/assets/lesson3/hr.png"
+import company_pic from "../../../src/assets/lesson3/company.png"
+import which_pic from "../../../src/assets/lesson3/which.png"
+import police_pic from "../../../src/assets/lesson3/police.png"
+import businessman_pic from "../../../src/assets/lesson3/businessman.png"
+import businesswoman_pic from "../../../src/assets/lesson3/businesswoman.png"
+
 export const unit_3 = [
     {id:0,
     english:'Doctor',
     vietnamese:'Bác sĩ',
     audio:doctor,
     example:'Tôi là bác sĩ, thế còn em?',
-    picture:'1'
+    picture:doctor_pic
     },
     {id:1,
     english:'Teacher',
     vietnamese:'Giáo viên',
     audio:teacher,
     example:'Cô giáo nói tiếng việt',
-    picture:''
+    picture:teacher_pic
     },
     {id:2,
     english:'Nurse',
     vietnamese:'Y tá',
     audio:nurse,
     example:'Cô y tá rất tốt bụng',
-    picture:'2'
+    picture:nurse_pic
     },
     {id:3,
     english:'Engineer',
     vietnamese:'Kỹ sư',
     audio:engineer,
     example:'Kỹ sư xây nhà',
-    picture:'3'
+    picture:engineer_pic
     },
     {id:4,
     english:'Director',
     vietnamese:'Giám đốc',
     audio:director,
     example:'Giám đốc rất nghiêm khắc',
-    picture:'4'
+    picture:director_pic
     },        
     {id:5,
     english:'What is your job?',
     vietnamese:'Làm nghề gì?',
     audio:your_job,
     example:'Chào chị, xin lỗi chị làm nghề gì ạ?',
-    picture:'5'
+    picture: your_job_pic
     },
     {id:6,
     english:'Do/work',
     vietnamese:'Làm',
     audio:do_work,
     example:'Chị đang làm cho công ty nào ạ?',
-    picture:'6'
+    picture:do_work_pic
     },
     {id:7,
     english:'Student',
     vietnamese:'Sinh viên',
     audio:student,
     example:'Học sinh đã hoàn thành kỳ nghỉ hè của họ',
-    picture:'7'
+    picture:student_pic
     },
     {id:8,
     english:'Pupil',
     vietnamese:'Học sinh',
     audio:pupil,
     example:'Học sinh đeo khẩu trang',
-    picture:'8'
+    picture:pupil_pic
     },
     {id:9,
     english:'Staff',
     vietnamese:'Nhân viên',
     audio:staff,
     example:'Tất cả nhân viên gặp nhau vào buổi tối',
-    picture:'9'
+    picture:staff_pic
     },
     {id:10,
     english:'Accountant',
     vietnamese:'Kế toán viên',
     audio:accountant,
     example:'Kế toán trả lương',
-    picture:''
+    picture:accountant_pic
     },
     {id:11,
     english:'Manager',
     vietnamese:'Quản lý',
     audio:manager,
     example:'Quản lý mặc đồng phục đặc biệt',
-    picture:''
+    picture:manager_pic
     },
     {id:12,
     english:'Human resources',
     vietnamese:'Nhân sự',
     audio:resources,
     example:'Phòng nhân sự đang tìm kiếm lập trình viên',
-    picture:''
+    picture:resources_pic
     },
     {id:13,
     english:'Company',
     vietnamese:'Công ty',
     audio:company,
     example:'Các công ty CNTT đầy hứa hẹn cho nền kinh tế',
-    picture:''
+    picture:company_pic
     },
     {id:14,
     english:'Which',
     vietnamese:'Nào/ cái nào',
     audio:which,
     example:'Cái nào là của bạn?',
-    picture:''
+    picture:which_pic
     },
     {id:15,
     english:'Police',
     vietnamese:'Công an',
     audio:police,
     example:'Công an bắt tội phạm nguy hiểm',
-    picture:''
+    picture:police_pic
     },
     {id:16,
     english:'Businessman',
     vietnamese:'Nam doanh nhân/ kinh doanh',
     audio:businessman,
     example:'Doanh nhân có nhiều cuộc họp kinh doanh',
-    picture:''
+    picture:businessman_pic
     },
     {id:17,
     english:'Businesswoman',
     vietnamese:'Nữ doanh nhân / kinh doanh',
     audio:businesswoman,
     example:'Doanh nhân có nhiều cuộc họp kinh doanh',
-    picture:''
+    picture:businesswoman_pic
     },
 ]
