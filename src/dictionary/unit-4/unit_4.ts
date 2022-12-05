ec07541688896c923d012394f30e28eff30c66d3
StarinovOleg <hromoi@inbox.ru>
2022-12-05 17:34:26 +0700

    add picture to unit4-unit-5

--- src/dictionary/unit-4/unit_4.ts
+++ src/dictionary/unit-4/unit_4.ts
@@ -17,25 +17,46 @@ import sir from "../../../src/assets/sounds/lesson4/sir.mp3"
 import ten from "../../../src/assets/sounds/lesson4/ten.mp3"
 import this_year from "../../../src/assets/sounds/lesson4/this_year.mp3"
 import year_old from "../../../src/assets/sounds/lesson4/year_old.mp3"
+
+import how_old_pic from "../../../src/assets/lesson4/how_old.png"
+import respect_pic from "../../../src/assets/lesson4/polite.png"
+import director_pic from "../../../src/assets/lesson4/director.png"
+import eight_pic from "../../../src/assets/lesson4/eight.png"
+import have_has_pic from "../../../src/assets/lesson4/have.png"
+import how_many_people_pic from "../../../src/assets/lesson4/many_people.png"
+import internal_meeting_pic from "../../../src/assets/lesson4/meet.png"
+import meet_pic from "../../../src/assets/lesson4/meet.png"
+import my_son_pic from "../../../src/assets/lesson4/son.png"
+import new_new_pic from "../../../src/assets/lesson4/new_year.png"
+import number_pic from "../../../src/assets/lesson4/numbers.png"
+import prepare_pic from "../../../src/assets/lesson4/prepare.png"
+import president_pic from "../../../src/assets/lesson4/president.png"
+import secretary_pic from "../../../src/assets/lesson4/secretary.png"
+import secured_notebook_pic from "../../../src/assets/lesson4/notebook.png"
+import sir_pic from "../../../src/assets/lesson4/sir.png"
+import ten_pic from "../../../src/assets/lesson4/ten.png"
+import this_year_pic from "../../../src/assets/lesson4/this_year.png"
+import year_old_pic from "../../../src/assets/lesson4/year_old.png"
+
 export const unit_4=[
-    {id: 0, english: "How old", vietnamese: "Bao nhiêu tuổi?", example:"Bao nhiêu tuổi?", audio:how_old, picture:""},
-    {id: 1, english: "Polite talk", vietnamese: "Con", example:"Con chào cô ạ", audio:'', picture:""},
-    {id: 2, english: "Respectfull attitude", vietnamese: "Ạ", example:"Con chào cô ạ", audio:respect, picture:""},
-    {id: 3, english: "New", vietnamese: "Mới", example:"Chúc mừng năm mới!", audio:new_new, picture:""},
-    {id: 4, english: "This year", vietnamese: "Năm nay", example:"Chúng tôi sẽ đi nghỉ trong năm nay", audio:this_year, picture:""},
-    {id: 5, english: "My son/boy", vietnamese: "Con trai", example:"Con trai tôi thích thể thao", audio:my_son, picture:""},
-    {id: 6, english: "Year old/ age", vietnamese: "Tuổi", example:"Từ 1-2 tuổi", audio:year_old, picture:""},
-    {id: 7, english: " Eight", vietnamese: "Tám", example:"Anh ấy đã làm việc được tám năm", audio:eight, picture:""},
-    {id: 8, english: "Ten", vietnamese: "Mười", example:"Anh ấy đã làm việc được mười năm", audio:ten, picture:""},
-    {id: 9, english: "Number", vietnamese: "Số đếm", example:"Số đếm tám", audio:number, picture:""},
-    {id: 10, english: "Have/has", vietnamese: "Có", example:"Bạn có bút không", audio:have_has, picture:""},
-    {id: 11, english: "Internal meeting", vietnamese: "Họp nội bộ", example:"Gặp tôi tại cuộc họp quản lý nội bộ", audio:internal_meeting, picture:""},
-    {id: 12, english: "How many people", vietnamese: "Mấy người", example:"Mấy người", audio:how_many_people, picture:""},
-    {id: 13, english: "Meet", vietnamese: "Họp", example:"Gặp tôi tại cuộc họp quản lý nội bộ", audio:meet, picture:""},
-    {id: 14, english: "Prepare", vietnamese: "Chuẩn bị", example:"Chuẩn bị bút của bạn", audio:prepare, picture:""},
-    {id: 15, english: "Secured notebook", vietnamese: "Sổ tay bảo mật", example:"Bảo vệ dữ liệu trong sổ ghi chép an toàn", audio:secured_notebook, picture:""},
-    {id: 16, english: "Sir!", vietnamese: "Thưa ngài", example:"Thưa ngài, tôi đã đến", audio:sir, picture:""},
-    {id: 17, english: "Secretary", vietnamese: "Thư ký", example:"Thư ký đã hẹn", audio:secretary, picture:""},
-    {id: 18, english: "President/chairman", vietnamese: "Chú tịch", example:"Tổng thống phát biểu trên truyền hình", audio:president, picture:""},
-    {id: 19, english: "Director", vietnamese: "Sếp/ giám đốc", example:"Giám đốc nghiêm khắc", audio:director, picture:""},
+    {id: 0, english: "How old", vietnamese: "Bao nhiêu tuổi?", example:"Bao nhiêu tuổi?", audio:how_old, picture:how_old_pic},
+    {id: 1, english: "Polite talk", vietnamese: "Con", example:"Con chào cô ạ", audio:'', picture:respect_pic},
+    {id: 2, english: "Respectfull attitude", vietnamese: "Ạ", example:"Con chào cô ạ", audio:respect, picture:respect_pic},
+    {id: 3, english: "New", vietnamese: "Mới", example:"Chúc mừng năm mới!", audio:new_new, picture:new_new_pic},
+    {id: 4, english: "This year", vietnamese: "Năm nay", example:"Chúng tôi sẽ đi nghỉ trong năm nay", audio:this_year, picture:this_year_pic},
+    {id: 5, english: "My son/boy", vietnamese: "Con trai", example:"Con trai tôi thích thể thao", audio:my_son, picture:my_son_pic},
+    {id: 6, english: "Year old/ age", vietnamese: "Tuổi", example:"Từ 1-2 tuổi", audio:year_old, picture:year_old_pic},
+    {id: 7, english: " Eight", vietnamese: "Tám", example:"Anh ấy đã làm việc được tám năm", audio:eight, picture:eight_pic},
+    {id: 8, english: "Ten", vietnamese: "Mười", example:"Anh ấy đã làm việc được mười năm", audio:ten, picture:ten_pic},
+    {id: 9, english: "Number", vietnamese: "Số đếm", example:"Số đếm tám", audio:number, picture:number_pic},
+    {id: 10, english: "Have/has", vietnamese: "Có", example:"Bạn có bút không", audio:have_has, picture:have_has_pic},
+    {id: 11, english: "Internal meeting", vietnamese: "Họp nội bộ", example:"Gặp tôi tại cuộc họp quản lý nội bộ", audio:internal_meeting, picture:internal_meeting_pic},
+    {id: 12, english: "How many people", vietnamese: "Mấy người", example:"Mấy người", audio:how_many_people, picture:how_many_people_pic},
+    {id: 13, english: "Meet", vietnamese: "Họp", example:"Gặp tôi tại cuộc họp quản lý nội bộ", audio:meet, picture:meet_pic},
+    {id: 14, english: "Prepare", vietnamese: "Chuẩn bị", example:"Chuẩn bị bút của bạn", audio:prepare, picture:prepare_pic},
+    {id: 15, english: "Secured notebook", vietnamese: "Sổ tay bảo mật", example:"Bảo vệ dữ liệu trong sổ ghi chép an toàn", audio:secured_notebook, picture:secured_notebook_pic},
+    {id: 16, english: "Sir!", vietnamese: "Thưa ngài", example:"Thưa ngài, tôi đã đến", audio:sir, picture:sir_pic},
+    {id: 17, english: "Secretary", vietnamese: "Thư ký", example:"Thư ký đã hẹn", audio:secretary, picture:secretary_pic},
+    {id: 18, english: "President/chairman", vietnamese: "Chú tịch", example:"Tổng thống phát biểu trên truyền hình", audio:president, picture:president_pic},
+    {id: 19, english: "Director", vietnamese: "Sếp/ giám đốc", example:"Giám đốc nghiêm khắc", audio:director, picture:director_pic},
 ]
--- src/dictionary/unit-5/unit_5.ts
+++ src/dictionary/unit-5/unit_5.ts
@@ -19,25 +19,46 @@ import wait from "../../../src/assets/sounds/lesson5/wait.mp3"
 import want from "../../../src/assets/sounds/lesson5/want.mp3"
 import welcome from "../../../src/assets/sounds/lesson5/welcome.mp3"
 
+import a_minute_pic from "../../../src/assets/lesson5/little.png"
+import buy_pic from "../../../src/assets/lesson5/buy.png"
+import cold_tea_pic from "../../../src/assets/lesson5/cold_tea.png"
+import cup_pic from "../../../src/assets/lesson5/cup.png"
+import currency_pic from "../../../src/assets/lesson5/money.png"
+import dish_pic from "../../../src/assets/lesson5/dish.png"
+import eat_pic from "../../../src/assets/lesson5/eat.png"
+import how_much_pic from "../../../src/assets/lesson5/how_much.png"
+import kilogram_pic from "../../../src/assets/lesson5/kilogram.png"
+import mango_pic from "../../../src/assets/lesson5/mango.png"
+import money_pic from "../../../src/assets/lesson5/money.png"
+import over_there_pic from "../../../src/assets/lesson5/over_there.png"
+import please_pic from "../../../src/assets/lesson5/please.png"
+import price_pic from "../../../src/assets/lesson5/price.png"
+import rice_noodle_pic from "../../../src/assets/lesson5/rice_noodle.png"
+import sell_pic from "../../../src/assets/lesson5/sell.png"
+import unit_pic from "../../../src/assets/lesson5/unit.png"
+import wait_pic from "../../../src/assets/lesson5/wait.png"
+import want_pic from "../../../src/assets/lesson5/want.png"
+import welcome_pic from "../../../src/assets/lesson5/welcome.png"
+
 export const unit_5=[
-    {id: 0, english: "How much", vietnamese: "Bao nhiêu tiền", example:"Bao nhiêu tiền", audio:how_much, picture:""},
-    {id: 1, english: "Want", vietnamese: "Muốn", example:"Tôi muốn mua xoài", audio:want, picture:""},
-    {id: 2, english: "Buy", vietnamese: "Mua", example:"Tôi muốn mua xoài", audio:buy, picture:""},
-    {id: 3, english: "Mango", vietnamese: "Xoài", example:"Tôi muốn mua xoài", audio:mango, picture:""},
-    {id: 4, english: "Kilogram", vietnamese: "Cân", example:"Một ký xoài", audio:kilogram, picture:""},
-    {id: 5, english: "Over there", vietnamese: "Ở đằng kia", example:"Văn phòng ở đằng kia", audio:over_there, picture:""},
-    {id: 6, english: "VN currency", vietnamese: "Đồng", example:"100000 Đồng", audio:currency, picture:""},
-    {id: 7, english: "Cost/price", vietnamese: "Trị giá", example:"Giá kem 15.000 đồng", audio:price, picture:""},
-    {id: 8, english: "Sell", vietnamese: "Bán", example:"50% Bán", audio:sell, picture:""},
-    {id: 9, english: "Money", vietnamese: "Tiền", example:"Tiền của tôi", audio:money, picture:""},
-    {id: 10, english: "Welcome", vietnamese: "Mời vào", example:"Mời vào", audio:welcome, picture:""},
-    {id: 11, english: "Eat", vietnamese: "Ăn", example:"Ăn cơm", audio:eat, picture:""},
-    {id: 12, english: "Dish", vietnamese: "Món", example:"Tôi thích đồ ăn Thái", audio:dish, picture:""},
-    {id: 13, english: "Rice noodle", vietnamese: "Bún chả", example:"Bún chả", audio:rice_noodle, picture:""},
-    {id: 14, english: "Unit", vietnamese: "Suất", example:"Suất 1", audio:unit, picture:""},
-    {id: 15, english: "Cup", vietnamese: "Cốc", example:"Một cốc cà phê", audio:cup, picture:""},
-    {id: 16, english: "Cold tea", vietnamese: "Trà đá", example:"Cốc trà đá", audio:cold_tea, picture:""},
-    {id: 17, english: "Wait", vietnamese: "Chờ", example:"Chờ tôi", audio:wait, picture:""},
-    {id: 18, english: "Please", vietnamese: "Làm ơn", example:"Làm ơn đợi tôi", audio:please, picture:""},
-    {id: 19, english: "A little/a minute", vietnamese: "Một chút", example:"Chờ một chút", audio:a_minute, picture:""},
+    {id: 0, english: "How much", vietnamese: "Bao nhiêu tiền", example:"Bao nhiêu tiền", audio:how_much, picture:how_much_pic},
+    {id: 1, english: "Want", vietnamese: "Muốn", example:"Tôi muốn mua xoài", audio:want, picture:want_pic},
+    {id: 2, english: "Buy", vietnamese: "Mua", example:"Tôi muốn mua xoài", audio:buy, picture:buy_pic},
+    {id: 3, english: "Mango", vietnamese: "Xoài", example:"Tôi muốn mua xoài", audio:mango, picture:mango_pic},
+    {id: 4, english: "Kilogram", vietnamese: "Cân", example:"Một ký xoài", audio:kilogram, picture:kilogram_pic},
+    {id: 5, english: "Over there", vietnamese: "Ở đằng kia", example:"Văn phòng ở đằng kia", audio:over_there, picture:over_there_pic},
+    {id: 6, english: "VN currency", vietnamese: "Đồng", example:"100000 Đồng", audio:currency, picture:currency_pic},
+    {id: 7, english: "Cost/price", vietnamese: "Trị giá", example:"Giá kem 15.000 đồng", audio:price, picture:price_pic},
+    {id: 8, english: "Sell", vietnamese: "Bán", example:"50% Bán", audio:sell, picture:sell_pic},
+    {id: 9, english: "Money", vietnamese: "Tiền", example:"Tiền của tôi", audio:money, picture:money_pic},
+    {id: 10, english: "Welcome", vietnamese: "Mời vào", example:"Mời vào", audio:welcome, picture:welcome_pic},
+    {id: 11, english: "Eat", vietnamese: "Ăn", example:"Ăn cơm", audio:eat, picture:eat_pic},
+    {id: 12, english: "Dish", vietnamese: "Món", example:"Tôi thích đồ ăn Thái", audio:dish, picture:dish_pic},
+    {id: 13, english: "Rice noodle", vietnamese: "Bún chả", example:"Bún chả", audio:rice_noodle, picture:rice_noodle_pic},
+    {id: 14, english: "Unit", vietnamese: "Suất", example:"Suất 1", audio:unit, picture:unit_pic},
+    {id: 15, english: "Cup", vietnamese: "Cốc", example:"Một cốc cà phê", audio:cup, picture:cup_pic},
+    {id: 16, english: "Cold tea", vietnamese: "Trà đá", example:"Cốc trà đá", audio:cold_tea, picture:cold_tea_pic},
+    {id: 17, english: "Wait", vietnamese: "Chờ", example:"Chờ tôi", audio:wait, picture:wait_pic},
+    {id: 18, english: "Please", vietnamese: "Làm ơn", example:"Làm ơn đợi tôi", audio:please, picture:please_pic},
+    {id: 19, english: "A little/a minute", vietnamese: "Một chút", example:"Chờ một chút", audio:a_minute, picture:a_minute_pic},
 ]
