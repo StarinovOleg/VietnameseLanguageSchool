71ac73a5b664837f7cce2831af630919a4cb55be
StarinovOleg <hromoi@inbox.ru>
2022-12-09 09:04:30 +0700

    add picture to unit14-15

--- src/dictionary/unit-15/unit_15.ts
+++ src/dictionary/unit-15/unit_15.ts
@@ -33,40 +33,74 @@ import still from "../../../src/assets/sounds/lesson15/still.mp3"
 import therefore from "../../../src/assets/sounds/lesson15/therefore.mp3"
 import understand from "../../../src/assets/sounds/lesson15/understand.mp3"
 
+import apartment_pic from "../../../src/assets/lesson15/apartment_pic.png"
+import bamboo_shoots_pic from "../../../src/assets/lesson15/bamboo_shoots_pic.png"
+import branch_pic from "../../../src/assets/lesson15/branch_pic.png"
+import cake_pic from "../../../src/assets/lesson15/cake_pic.png"
+import come_back_pic from "../../../src/assets/lesson15/come_back_pic.png"
+import communicate_pic from "../../../src/assets/lesson15/communicate_pic.png"
+import culture_pic from "../../../src/assets/lesson15/culture_pic.png"
+import directly_pic from "../../../src/assets/lesson15/directly_pic.png"
+import discover_pic from "../../../src/assets/lesson15/discover_pic.png"
+import english_america_pic from "../../../src/assets/lesson15/english_america_pic.png"
+import exciting_pic from "../../../src/assets/lesson15/exciting_pic.png"
+import family_pic from "../../../src/assets/lesson15/family_pic.png"
+import feel_pic from "../../../src/assets/lesson15/feel_pic.png"
+import firework_pic from "../../../src/assets/lesson15/firework_pic.png"
+import grateful_pic from "../../../src/assets/lesson15/grateful_pic.png"
+import have_success_pic from "../../../src/assets/lesson15/have_success_pic.png"
+import how_about_you_pic from "../../../src/assets/lesson15/how_about_you_pic.png"
+import is_for_more_pic from "../../../src/assets/lesson15/is_for_more_pic.png"
+import language_pic from "../../../src/assets/lesson15/language_pic.png"
+import local_pic from "../../../src/assets/lesson15/local_pic.png"
+import long_time_pic from "../../../src/assets/lesson15/long_time_pic.png"
+import moment_pic from "../../../src/assets/lesson15/moment_pic.png"
+import new_years_pic from "../../../src/assets/lesson15/firework_pic.png"
+import next_vocation_pic from "../../../src/assets/lesson15/next_vocation_pic.png"
+import plan_pic from "../../../src/assets/lesson15/plan_pic.png"
+import rental_house_pic from "../../../src/assets/lesson15/rental_house_pic.png"
+import rice_cake_pic from "../../../src/assets/lesson15/rice_cake_pic.png"
+import rice_pic from "../../../src/assets/lesson15/rice_pic.png"
+import spirit_pic from "../../../src/assets/lesson15/spirit_pic.png"
+import spring_roll_pic from "../../../src/assets/lesson15/spring_roll_pic.png"
+import state_pic from "../../../src/assets/lesson15/state_pic.png"
+import still_pic from "../../../src/assets/lesson15/still_pic.png"
+import therefore_pic from "../../../src/assets/lesson15/therefore_pic.png"
+import understand_pic from "../../../src/assets/lesson15/understand_pic.png"
 
 export const unit_15=[
-    {id: 0, english: "Have success", vietnamese: "Cầu thành công", example:"Bạn đã thành công", audio:have_success, picture:""},
-    {id: 1, english: "Next vacation", vietnamese: "Kỳ nghỉ tới", example:"Kỳ nghỉ tiếp theo trong năm mới", audio:next_vocation, picture:""},
-    {id: 2, english: "Directly", vietnamese: "Trực tiếp", example:"Trực tiếp", audio:directly, picture:""},
-    {id: 3, english: "Therefore", vietnamese: "Chính vì vậy", example:"Vì vậy tôi thông minh", audio:therefore, picture:""},
-    {id: 4, english: "Discover", vietnamese: "Khám phá", example:"Khám phá khủng long", audio:discover, picture:""},
-    {id: 5, english: "What is for more", vietnamese: "Mục đích gì nữa", example:"Mục đích gì nữa", audio:is_for_more, picture:""},
-    {id: 6, english: "Long time", vietnamese: "Thời gian lâu", example:"Rất lâu trước đó", audio:long_time, picture:""},
-    {id: 7, english: "Local", vietnamese: "Người bản đị̉a", example:"Người bản đị̉a", audio:local, picture:""},
-    {id: 8, english: "Culture", vietnamese: "Văn hóa", example:"Văn hóa Việt Nam", audio:culture, picture:""},
-    {id: 9, english: "Communicate", vietnamese: "Nói chuyện", example:"Nói chuyện", audio:communicate, picture:""},
-    {id: 10, english: "Grateful", vietnamese: "Biết ơn", example:"Con trai biết ơn", audio:grateful, picture:""},
-    {id: 11, english: "Still", vietnamese: "Vẫn", example:"Vẫn yêu bạn", audio:still, picture:""},
-    {id: 12, english: "Language", vietnamese: "Ngôn ngữ", example:"Ngôn ngữ tiếng Việt", audio:language, picture:""},
-    {id: 13, english: "English America", vietnamese: "Anh Mỹ", example:"Anh Mỹ", audio:english_america, picture:""},
-    {id: 14, english: "Come back", vietnamese: "Trở lại", example:"Quay lại sớm", audio:come_back, picture:""},
-    {id: 15, english: "Family", vietnamese: "Gia đình", example:"Chàng trai của gia đình", audio:family, picture:""},
-    {id: 16, english: "Understand everything", vietnamese: "Biết hết", example:"Biết hết", audio:understand, picture:""},
-    {id: 17, english: "Rental house", vietnamese: "Thuê nhà", example:"Thuê nhà", audio:rental_house, picture:""},
-    {id: 18, english: "Plan/ intention", vietnamese: "Dự định /kế hoạch", example:"kế hoạch", audio:plan, picture:""},
-    {id: 19, english: "Apartment", vietnamese: "Chung cư", example:"Chung cư", audio:apartment, picture:""},
-    {id: 20, english: "Feel/ understand", vietnamese: "Cảm nhận", example:"Cảm nhận", audio:feel, picture:""},
-    {id: 21, english: "How about you", vietnamese: "Thì sao ạ", example:"Thì sao ạ", audio:how_about_you, picture:""},
-    {id: 22, english: "Chung cake", vietnamese: "Bánh chưng", example:"Bánh chưng", audio:cake, picture:""},
-    {id: 23, english: "Rice cake", vietnamese: "Bánh dày", example:"Bánh dày", audio:rice_cake, picture:""},
-    {id: 24, english: "Spring roll", vietnamese: "Nem", example:"Spring roll", audio:spring_roll, picture:""},
-    {id: 25, english: "Sticky rice", vietnamese: "Xôi", example:"Sticky rice", audio:rice, picture:""},
-    {id: 26, english: "Bamboo shoots", vietnamese: "Canh măng", example:"Bamboo shoots", audio:bamboo_shoots, picture:""},
-    {id: 27, english: "New years Eve", vietnamese: "Giao thừa", example:"New years Eve", audio:new_years, picture:""},
-    {id: 28, english: "The moment", vietnamese: "Khoảnh khac", example:"Khoảnh khắc thật tuyệt", audio:moment, picture:""},
-    {id: 29, english: "Spirit", vietnamese: "Thiêng liêng", example:"Thiêng liêng", audio:spirit, picture:""},
-    {id: 30, english: "Firework", vietnamese: "Pháo hoà", example:"Pháo hoa đẹp", audio:firework, picture:""},
-    {id: 31, english: "Exciting", vietnamese: "Thú vị", example:"Tôi thú vị", audio:exciting, picture:""},
-    {id: 32, english: "State", vietnamese: "Bang", example:"Bang", audio:state, picture:""},
-    {id: 33, english: "Branch", vietnamese: " Thậ", example:"Git thậ", audio:branch, picture:""},
+    {id: 0, english: "Have success", vietnamese: "Cầu thành công", example:"Bạn đã thành công", audio:have_success, picture:have_success_pic},
+    {id: 1, english: "Next vacation", vietnamese: "Kỳ nghỉ tới", example:"Kỳ nghỉ tiếp theo trong năm mới", audio:next_vocation, picture:next_vocation_pic},
+    {id: 2, english: "Directly", vietnamese: "Trực tiếp", example:"Trực tiếp", audio:directly, picture:directly_pic},
+    {id: 3, english: "Therefore", vietnamese: "Chính vì vậy", example:"Vì vậy tôi thông minh", audio:therefore, picture:therefore_pic},
+    {id: 4, english: "Discover", vietnamese: "Khám phá", example:"Khám phá khủng long", audio:discover, picture:discover_pic},
+    {id: 5, english: "What is for more", vietnamese: "Mục đích gì nữa", example:"Mục đích gì nữa", audio:is_for_more, picture:is_for_more_pic},
+    {id: 6, english: "Long time", vietnamese: "Thời gian lâu", example:"Rất lâu trước đó", audio:long_time, picture:long_time_pic},
+    {id: 7, english: "Local", vietnamese: "Người bản đị̉a", example:"Người bản đị̉a", audio:local, picture:local_pic},
+    {id: 8, english: "Culture", vietnamese: "Văn hóa", example:"Văn hóa Việt Nam", audio:culture, picture:culture_pic},
+    {id: 9, english: "Communicate", vietnamese: "Nói chuyện", example:"Nói chuyện", audio:communicate, picture:communicate_pic},
+    {id: 10, english: "Grateful", vietnamese: "Biết ơn", example:"Con trai biết ơn", audio:grateful, picture:grateful_pic},
+    {id: 11, english: "Still", vietnamese: "Vẫn", example:"Vẫn yêu bạn", audio:still, picture:still_pic},
+    {id: 12, english: "Language", vietnamese: "Ngôn ngữ", example:"Ngôn ngữ tiếng Việt", audio:language, picture:language_pic},
+    {id: 13, english: "English America", vietnamese: "Anh Mỹ", example:"Anh Mỹ", audio:english_america, picture:english_america_pic},
+    {id: 14, english: "Come back", vietnamese: "Trở lại", example:"Quay lại sớm", audio:come_back, picture:come_back_pic},
+    {id: 15, english: "Family", vietnamese: "Gia đình", example:"Chàng trai của gia đình", audio:family, picture:family_pic},
+    {id: 16, english: "Understand everything", vietnamese: "Biết hết", example:"Biết hết", audio:understand, picture:understand_pic},
+    {id: 17, english: "Rental house", vietnamese: "Thuê nhà", example:"Thuê nhà", audio:rental_house, picture:rental_house_pic},
+    {id: 18, english: "Plan/ intention", vietnamese: "Dự định /kế hoạch", example:"kế hoạch", audio:plan, picture:plan_pic},
+    {id: 19, english: "Apartment", vietnamese: "Chung cư", example:"Chung cư", audio:apartment, picture:apartment_pic},
+    {id: 20, english: "Feel/ understand", vietnamese: "Cảm nhận", example:"Cảm nhận", audio:feel, picture:feel_pic},
+    {id: 21, english: "How about you", vietnamese: "Thì sao ạ", example:"Thì sao ạ", audio:how_about_you, picture:how_about_you_pic},
+    {id: 22, english: "Chung cake", vietnamese: "Bánh chưng", example:"Bánh chưng", audio:cake, picture:cake_pic},
+    {id: 23, english: "Rice cake", vietnamese: "Bánh dày", example:"Bánh dày", audio:rice_cake, picture:rice_cake_pic},
+    {id: 24, english: "Spring roll", vietnamese: "Nem", example:"Spring roll", audio:spring_roll, picture:spring_roll_pic},
+    {id: 25, english: "Sticky rice", vietnamese: "Xôi", example:"Sticky rice", audio:rice, picture:rice_pic},
+    {id: 26, english: "Bamboo shoots", vietnamese: "Canh măng", example:"Bamboo shoots", audio:bamboo_shoots, picture:bamboo_shoots_pic},
+    {id: 27, english: "New years Eve", vietnamese: "Giao thừa", example:"New years Eve", audio:new_years, picture:new_years_pic},
+    {id: 28, english: "The moment", vietnamese: "Khoảnh khac", example:"Khoảnh khắc thật tuyệt", audio:moment, picture:moment_pic},
+    {id: 29, english: "Spirit", vietnamese: "Thiêng liêng", example:"Thiêng liêng", audio:spirit, picture:spirit_pic},
+    {id: 30, english: "Firework", vietnamese: "Pháo hoà", example:"Pháo hoa đẹp", audio:firework, picture:firework_pic},
+    {id: 31, english: "Exciting", vietnamese: "Thú vị", example:"Tôi thú vị", audio:exciting, picture:exciting_pic},
+    {id: 32, english: "State", vietnamese: "Bang", example:"Bang", audio:state, picture:state_pic},
+    {id: 33, english: "Branch", vietnamese: " Thậ", example:"Git thậ", audio:branch, picture:branch_pic},
 ]
