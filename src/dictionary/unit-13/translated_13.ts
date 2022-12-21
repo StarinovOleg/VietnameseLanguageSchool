c0e4df5db525808836302869ccb6b7ef588ebaf5
StarinovOleg <hromoi@inbox.ru>
2022-12-21 09:38:35 +0700

    add all translate sentence for elementary level

---
+++ src/dictionary/unit-13/translated_13.ts
@@ -0,0 +1,61 @@
+export const trans_13_1=[
+  {
+    id:1,
+    english:'I have got some recommendations',
+    vietnamese:'Tôi vừa nhận được vài lời gợi ý',
+    user_message:''
+  },
+  {
+    id:2,
+    english:'Dear darling! Can you help mama?',
+    vietnamese:'Con yêu! Còn có thể giúp mẹ không?',
+    user_message:''
+  },
+  {
+    id:3,
+    english:'Today, we had one difficult problem and nobody could fix it',
+    vietnamese:'Hôm nay, chúng tôi gặp một vấn đề khó và không ai có thể giải được',
+    user_message:''
+  },
+  {
+    id:4,
+    english:'Can you suggest me something on the menu?',
+    vietnamese:'Bạn có thể gợi ý cho tôi một cái gì đó trên menu?',
+    user_message:''
+  }, 
+
+]
+
+export const trans_13_2=[
+  {
+    id:1,
+    english:'I will have a speech on tomorrow morning',
+    vietnamese:'Tôi có một bài phát biểu vào sáng ngày mai',
+    user_message:''
+  },
+  {
+    id:2,
+    english:'My foreigner partner comes from Korea',
+    vietnamese:'Đối tác nước ngoài của tôi đến từ Hàn Quốc',
+    user_message:''
+  },
+  {
+    id:3,
+    english:'Where is your house? I live in Thanh Xuan',
+    vietnamese:'Nhà bạn ở đâu? Nhà tôi ở quận Thanh Xuân',
+    user_message:''
+  },
+  {
+    id:4,
+    english:'Can you pass me something? Of course',
+    vietnamese:'Bạn có thể chuyển cho tôi vài thứ được không? Tất nhiên rồi',
+    user_message:''
+  }, 
+  {
+    id:5,
+    english:'Where is your projector? It in the meeting room',
+    vietnamese:'Máy chiếu của bạn ở đâu? Nó ở trong phòng họp',
+    user_message:''
+  },  
+
+]
---
+++ src/dictionary/unit-9/translated_9.ts
@@ -0,0 +1,61 @@
+export const trans_9_1=[
+  {
+    id:1,
+    english:'What is the weather like in Da Lat today?',
+    vietnamese:'Thời tiết ở Đà Lạt hôm nay như thế nào?',
+    user_message:''
+  },
+  {
+    id:2,
+    english:'What is the weatcher like in winter?',
+    vietnamese:'Thời tiết mùa đông như thế nào?',
+    user_message:''
+  },
+  {
+    id:3,
+    english:'Today, the weatcher is very hot',
+    vietnamese:'Hôm nay, thời tiết rất nóng',
+    user_message:''
+  },
+  {
+    id:4,
+    english:'The sky is very black. It is going to rain',
+    vietnamese:'Bầu trời đang đến dần. Trời sắp mưa rồi',
+    user_message:''
+  }, 
+
+]
+
+export const trans_9_2=[
+  {
+    id:1,
+    english:'Do you like summer in Viet Nam? No, I do not like, beacause it is very hot',
+    vietnamese:'Bạn có thích mùa hè ở Việt Nam không? Tôi không thích vì nó rất nóng',
+    user_message:''
+  },
+  {
+    id:2,
+    english:'There are four seasons in Viet Nam. They are Spring, Summer, Fall and Winter',
+    vietnamese:'Ở Việt Nam có bốn mùa: Xuân, Hạ, Thu, Đồng',
+    user_message:''
+  },
+  {
+    id:3,
+    english:'Which season in viet Nma do you like best?',
+    vietnamese:'Bân tích mùa nào ở Việt Nam nhất?',
+    user_message:''
+  },
+  {
+    id:4,
+    english:'Today, the weatcher forecast says that it is going to rain',
+    vietnamese:'Hôm nay dự báo trời sẽ có mưa?',
+    user_message:''
+  }, 
+  {
+    id:5,
+    english:'I really love the weatcher in Da Lat',
+    vietnamese:'Tôi rất thích thời tiết ở Đà Lạt',
+    user_message:''
+  },  
+
+]
