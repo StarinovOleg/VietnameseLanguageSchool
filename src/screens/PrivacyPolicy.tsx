0c2a50398fc8223bfdb0a49a0db483c833a34544
StarinovOleg <57636385+StarinovOleg@users.noreply.github.com>
2023-03-18 09:35:33 +0700

    Update PrivacyPolicy.tsx

--- src/screens/PrivacyPolicy.tsx
+++ src/screens/PrivacyPolicy.tsx
@@ -10,10 +10,12 @@ function PrivacyPolicy() {
           <p>This page is used to inform visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service.</p>
           <p>If you choose to use our Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that we collect is used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.</p>
           <p>The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which are accessible at Tu Lingua unless otherwise defined in this Privacy Policy.</p>
-        <P children="Information Collection and Use"/>
-      <p></p> 
-      <p></p> 
-      <p></p>
+        
+          <p className="text-xl font-semibold">Information Collection and Use</p>
+          <p>For a better experience, while using our Service, we may require you to provide us with certain personally identifiable information, including but not limited to I do not collect data. The information that we request will be retained by us and used as described in this privacy policy.</p> 
+          <p>The app does use third-party services that may collect information used to identify you.</p> 
+          <p>Link to the privacy policy of third-party service providers used by the app</p>
+          <a href="https://www.google.com/policies/privacy/">* Google Play Services</a>
         
       <p className="text-xl font-semibold">Log Data</p>
       <p></p>
