67f81c851c3ef627494c8bba2bcc5a055b5ae74c
StarinovOleg <57636385+StarinovOleg@users.noreply.github.com>
2023-03-18 09:00:32 +0700

    Update footer.tsx
    flex

--- src/screens/main/footer/footer.tsx
+++ src/screens/main/footer/footer.tsx
@@ -5,7 +5,7 @@ import ScrollToTop from "../../../services/scroll-top.service";
 function Footer() {
   return (
     <footer className="bg-blue-700  bottom-0 w-full">
-      <div className="text-white text-xl">
+      <div className="text-white text-xl flex gap-6">
         {converstation.footer.company_name} {new Date().getFullYear()}{" "}
         {converstation.footer.web_app_name}
         <NavLink to="#" onClick={() => ScrollToTop}>
