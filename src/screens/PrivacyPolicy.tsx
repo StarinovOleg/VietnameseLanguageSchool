a392d0d42dbacc3204f92b906e41e844af81ba52
StarinovOleg <57636385+StarinovOleg@users.noreply.github.com>
2023-03-18 09:23:26 +0700

    add privacy

--- src/screens/PrivacyPolicy.tsx
+++ src/screens/PrivacyPolicy.tsx
@@ -1,10 +1,21 @@
 import React from "react";
-
+import H1 from "../../ui-library/H1";
+import P from "../../ui-library/P";
 function PrivacyPolicy() {
   return (
    
-      <div className="text-white text-xl flex gap-6 flex-wrap">
-       Here privacy policy
+      <div className="text-white text-xl">
+        <H1 children="Privacy-Police" />
+        <P children="Privacy Policy"/>
+        <P children="Information Collection and Use"/>
+        <P children="Log Data"/>
+        <P children="Cookies"/>
+        <P children="Service Providers"/>
+        <P children="Security"/>
+        <P children="Links to Other Sites"/>
+        <P children="Children’s Privacy"/>
+        <P children="Changes to This Privacy Policy"/>
+        <P children="Contact Us"/>
       </div>
     
   );
