26ff59ea22aaa1913dce179bd3bd8d32273b6739
StarinovOleg <57636385+StarinovOleg@users.noreply.github.com>
2023-03-18 08:56:55 +0700

    Update footer.tsx

--- src/screens/main/footer/footer.tsx
+++ src/screens/main/footer/footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { converstation } from "../../../store/static";
 import { NavLink } from "react-router-dom";
+import ScrollToTop from "../../../services/scroll-top.service";
 function Footer() {
   return (
     <footer className="bg-blue-700  bottom-0 w-full">
