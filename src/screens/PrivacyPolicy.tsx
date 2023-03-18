5dc8baca976cfe63507d93bc329944e71f615271
StarinovOleg <57636385+StarinovOleg@users.noreply.github.com>
2023-03-18 09:08:51 +0700

    Create PrivatePolicy screen
    PrivatePolicy

---
+++ src/screens/PrivacyPolicy.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function PrivacyPolice() {
+  return (
+   
+      <div className="text-white text-xl flex gap-6 flex-wrap">
+       Here privacy policy
+      </div>
+    
+  );
+}
+
+export default PrivacyPolice;
