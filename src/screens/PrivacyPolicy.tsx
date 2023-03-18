97a42732d3884c0b1eca9310d849c7c6506c425b
StarinovOleg <57636385+StarinovOleg@users.noreply.github.com>
2023-03-18 09:28:07 +0700

    Update PrivacyPolicy.tsx

--- src/screens/PrivacyPolicy.tsx
+++ src/screens/PrivacyPolicy.tsx
@@ -1,21 +1,36 @@
 import React from "react";
-import H1 from "../../ui-library/H1";
-import P from "../../ui-library/P";
+import H1 from "../../../ui-library/H1";
+import P from "../../../ui-library/P";
 function PrivacyPolicy() {
   return (
    
       <div className="text-white text-xl">
         <H1 children="Privacy-Police" />
         <P children="Privacy Policy"/>
+          <p>Jack Bean Software built the Tu Lingua app as a Free app. This SERVICE is provided by Jack Bean Software at no cost and is intended for use as is.</p>
+          <p>This page is used to inform visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service.</p>
+          <p>If you choose to use our Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that we collect is used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.</p>
+          <p>The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which are accessible at Tu Lingua unless otherwise defined in this Privacy Policy.</p>
         <P children="Information Collection and Use"/>
+      <p></p> 
+      <p></p> 
+      <p></p>
         <P children="Log Data"/>
+      <p></p>
         <P children="Cookies"/>
+      <p></p>
         <P children="Service Providers"/>
+      <p></p>
         <P children="Security"/>
+      <p></p>
         <P children="Links to Other Sites"/>
+      <p></p>
         <P children="Children’s Privacy"/>
+      <p></p>
         <P children="Changes to This Privacy Policy"/>
+      <p></p>
         <P children="Contact Us"/>
+      <p></p>
       </div>
     
   );
