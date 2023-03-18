88c13b34dadd3a1162335e700a9598c976e1b12f
StarinovOleg <57636385+StarinovOleg@users.noreply.github.com>
2023-03-18 08:51:30 +0700

    Update footer.tsx
    privacy policy link

--- src/screens/main/footer/footer.tsx
+++ src/screens/main/footer/footer.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { converstation } from "../../../store/static";
-
+import { NavLink } from "react-router-dom";
 function Footer() {
   return (
     <footer className="bg-blue-700  bottom-0 w-full">
       <div className="text-white text-xl">
         {converstation.footer.company_name} {new Date().getFullYear()}{" "}
         {converstation.footer.web_app_name}
+        <NavLink to="#" onClick={() => ScrollToTop}>
+            Privacy Policy
+        </NavLink>
       </div>
     </footer>
   );
