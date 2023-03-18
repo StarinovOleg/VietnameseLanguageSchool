6b5d47beb6397b0a234c01f21ade968485db6f80
StarinovOleg <57636385+StarinovOleg@users.noreply.github.com>
2023-03-18 09:46:59 +0700

    Update PrivacyPolicy.tsx

--- src/screens/PrivacyPolicy.tsx
+++ src/screens/PrivacyPolicy.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function PrivacyPolicy() {
   return (
    
-      <div className="text-xl">
+      <div className="text-xl m-10">
       <h1>Privacy Policy</h1>
           <p className="text-xl font-semibold">Privacy Policy</p>
           <p>Jack Bean Software built the Tu Lingua app as a Free app. This SERVICE is provided by Jack Bean Software at no cost and is intended for use as is.</p>
@@ -21,25 +21,35 @@ function PrivacyPolicy() {
       <p>We want to inform you that whenever you use our Service, in a case of an error in the app we collect data and information (through third-party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the app when utilizing our Service, the time and date of your use of the Service, and other statistics.</p>
        
       <p className="text-xl font-semibold">Cookies</p>
-      <p></p>
-        
+      <p>Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These are sent to your browser from the websites that you visit and are stored on your device's internal memory.</p>
+      <p>This Service does not use these “cookies” explicitly. However, the app may use third-party code and libraries that use “cookies” to collect information and improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.</p>
+      
       <p className="text-xl font-semibold">Service Providers</p>
-      <p></p>
-        
+      <p>We may employ third-party companies and individuals due to the following reasons:</p>
+      <ul>
+        <li>To facilitate our Service;</li>
+        <li>To provide the Service on our behalf;</li>
+        <li>To perform Service-related services; or</li>
+        <li>To assist us in analyzing how our Service is used.</li>
+      </ul>
+      <p>We want to inform users of this Service that these third parties have access to their Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.</p>
+      
       <p className="text-xl font-semibold">Security</p>
-      <p></p>
+      <p>We value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.</p>
       
       <p className="text-xl font-semibold">Links to Other Sites</p>
-      <p></p>
+      <p>This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.</p>
        
       <p className="text-xl font-semibold">Children’s Privacy</p>
-      <p></p>
+      <p>These Services do not address anyone under the age of 13. We do not knowingly collect personally identifiable information from children under 13 years of age. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to do the necessary actions.</p>
         
       <p className="text-xl font-semibold">Changes to This Privacy Policy</p>
-      <p></p>
-        
+      <p>We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page.</p>
+      <p>This policy is effective as of 2023-02-27</p> 
+      
       <p className="text-xl font-semibold">Contact Us</p>
-      <p></p>
+      <p>If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us at </p>
+      <a href="jackbeancode@gmail.com">jackbeancode@gmail.com</a>
       </div>
     
   );
